37c978e119bfcbda0f1887c865cdf85e3e87851e
majayaka <yumotoayaka@gmail.com>
2023-06-22 13:11:04 +0200

    contact remodified

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -29,11 +29,11 @@ const handleSubmit = async (e) => {
      <h2  className='text-center text-4xl font-bold text-[#414382] m-8 py-10'>CONTACTEZ-NOUS</h2>
     <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
         <div className='grid grid-cols-2 gap-2'>
-            <input className='border shadow-lg p-3' type="text" placeholder='Nom'/>
-            <input className='border shadow-lg p-3' type="email" placeholder='Email'/>
+            <input className='border shadow-lg p-3' type="text" name="name" placeholder='Nom'/>
+            <input className='border shadow-lg p-3' type="email" name="email" placeholder='Email'/>
         </div>
-        <input className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Sujet'/>
-        <textarea className='border shadow-lg p-3 w-full' cols="30" rows="10" placeholder='Message'></textarea>
+        <input className='border shadow-lg p-3 w-full my-2' type="text" name="subject" placeholder='Sujet'/>
+        <textarea className='border shadow-lg p-3 w-full' cols="30" rows="10" name="message" placeholder='Message'></textarea>
         <button className='border shadow-lg p-3 w-full mt-2 text-[#414382] font-bold text-xl'>ENVOYER</button>
     </form>
     </div>

9c5d210a5017d4e74633e3bd294e777cf138de6b
majayaka <yumotoayaka@gmail.com>
2023-06-23 10:09:47 +0200

    contact page modified

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -35,7 +35,7 @@ const handleSubmit = async (e) => {
 
   return (
     <div id='contact' className='max-w-[1240px] m-auto p-20 h-screen'>
-     <h2  className='text-center text-4xl font-bold text-[#414382] m-8 py-10'>CONTACTEZ-NOUS</h2>
+     <h2  className='text-center text-2xl sm:text-4xl font-bold text-[#414382] m-8 py-1 sd:py-10'>CONTACTEZ-NOUS</h2>
     <form className='max-w-[600px] m-auto' onSubmit={handleSubmit}>
         <div className='grid grid-cols-2 gap-2'>
             <input className='border shadow-lg p-3' type="text" name="name" placeholder='Nom'/>
